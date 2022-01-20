a2283de3c08c2876b27f2febb9548c98b152db39
AmandaGorni <ap.gorni@gmail.com>
2022-01-20 17:47:09 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `My blog`,
+    description: `a blog about frontend development and other cool stuff.`,
+    author: `@AmandaGorni`,
+    siteUrl: `https://github.com/AmandaGorni`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
